dade4677a267aa72c30b32b7a013fc9d18a0e45f
yushin1402 <s.masaomi16h@gmail.com>
2021-12-05 23:02:31 +0900

    first commit

---
+++ rdsapp/src/test/java/com/example/rdsapp/RdsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rdsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
